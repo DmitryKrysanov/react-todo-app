9d3e44ea72247522df9536652c535f295d5de317
Dima <dim.kry.lis@gmail.com>
2020-09-10 10:34:20 +0300

    avatar and menu have been added

--- src/components/Header/IsAuth/IsAuth.tsx
+++ src/components/Header/IsAuth/IsAuth.tsx
@@ -1,31 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import classes from './IsAuth.module.scss';
 
 interface Props {
     logOut: () => void
     resetTodos: () => void
 }
 
-const IsAuth = (props: Props): JSX.Element => {
+const IsAuth = (props: Props) => {
+
+    const avatarPlaceholder = "https://placehold.it/80";
+
+    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+
+    const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
 
     const onLogOut = () => {
         props.logOut();
         props.resetTodos();
     }
 
-        return (
-            <nav>
-                <ul>
-                    <li>
-                        <button
-                            className='btn btn__secondary'
-                            onClick={onLogOut}
-                        >
-                            Log Out
-                        </button>
-                    </li>
-                </ul>
-            </nav>
-        )
-    }
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <button className="btn" onClick={handleClick}>
+                        <img className={ classes.avatar } src={ avatarPlaceholder } alt="user menu" />
+                    </button>
+                    <Menu
+                        id="simple-menu"
+                        anchorEl={ anchorEl }
+                        keepMounted
+                        open={ Boolean(anchorEl) }
+                        onClose={ handleClose }
+                    >
+                        <MenuItem onClick={ handleClose }>Profile</MenuItem>
+                        <MenuItem onClick={ onLogOut }>Logout</MenuItem>
+                    </Menu>
+                </li>
+            </ul>
+        </nav>
+    )
+}
 
 export default IsAuth;
